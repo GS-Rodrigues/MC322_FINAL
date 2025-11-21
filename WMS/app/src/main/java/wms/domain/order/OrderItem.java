2a5ed629db7d36c2ffa0ec9f0627168afd6caf00
Guilherme da Silva Rodrigues <guilherme_silva_rodrigues@outlook.com.br>
2025-11-21 13:16:39 -0300

    Criado Classe OrderItem

---
+++ WMS/app/src/main/java/wms/domain/order/OrderItem.java
@@ -0,0 +1,44 @@
+package wms.domain.order;
+
+import wms.domain.entity.Product;
+
+/**
+ * Representa um item dentro de um pedido, contendo o produto e a quantidade.
+ * 
+ * @author Guilherme Rodrigues
+ * @version 1.0
+ * @since 2025-11-21
+ */
+public class OrderItem {
+    private Product product;
+    private int quantity;
+
+    /**
+     * Cria um novo OrderItem com o produto e a quantidade informados.
+     *
+     * @param product  o produto deste item
+     * @param quantity a quantidade do produto
+     */
+    public OrderItem(Product product, int quantity) {
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    /**
+     * Retorna a quantidade deste item.
+     *
+     * @return a quantidade
+     */
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    /**
+     * Retorna o produto associado a este item.
+     *
+     * @return o produto
+     */
+    public Product getProduct() {
+        return this.product;
+    }
+}
